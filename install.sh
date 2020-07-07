8bfbd53834182433e904b6b121f3c59d00fb0189
Omar Bazaid <omar.vip3@gmail.com>
2020-07-07 13:59:37 +0300

    edit bash script

--- install.sh
+++ install.sh
@@ -4,3 +4,5 @@ sudo apt install make -y
 sudo apt install docker.io -y 
 sudo curl -L "https://github.com/docker/compose/releases/download/1.26.1/docker-compose-$(uname -s)-$(uname -m)" -o /usr/local/bin/docker-compose
 sudo chmod +x /usr/local/bin/docker-compose
+sh -c "$(curl -fsSL https://raw.githubusercontent.com/ohmybash/oh-my-bash/master/tools/install.sh)" 
+reset
